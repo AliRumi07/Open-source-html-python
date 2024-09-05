e12cb035e3d46f9e6e485325ae8e8733780404b5
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-09-05 13:06:15 +0500

    Update btc_long.py

--- btc_long.py
+++ btc_long.py
@@ -33,8 +33,8 @@ price_precision = {
     # Add more pairs and their respective precisions here
 }
 
-tp_percentage = 0.002  # 0.2%
-sl_percentage = 0.002  # 0.2%
+tp_percentage = 0.004  # 0.4%
+sl_percentage = 0.004  # 0.4%
 
 async def get_symbol_price(symbol):
     async with websockets.connect(ws_uri) as websocket:
