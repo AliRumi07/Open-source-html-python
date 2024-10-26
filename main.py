e0f06495c60d956b4b2e2ef2fce1c3966e913d04
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-10-26 06:46:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '''
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Hello World</title>
-            <meta name="monetag" content="61ddc905b0747014cd45a71f3112eb4b">
-            <style>
-                body {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    margin: 0;
-                    font-family: Arial, sans-serif;
-                    background-color: #f0f0f0;
-                }
-                h1 {
-                    color: #333;
-                    font-size: 48px;
-                }
-            </style>
-            <script src="https://alwingulla.com/88/tag.min.js" data-zone="110367" async data-cfasync="false"></script>
-        </head>
-        <body>
-            <h1>Hello, World!</h1>
-        </body>
-    </html>
-    '''
+def show_app_ads():
+    try:
+        with open('app-ads.txt', 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return 'app-ads.txt file not found'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
