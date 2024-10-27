f122f6ff88a6b21b81687cf0e09ce74374f9b262
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-10-27 18:43:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from flask import Flask, render_template_string, Response
+from flask import Flask, render_template_string
 import threading
 import time
 import webbrowser
 
 app = Flask(__name__)
 
-# HTML template with push notification script
+# HTML template with auto-refresh meta tag and the new ad code
 html_template = '''
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="refresh" content="300">
-    <title>Push Notification Page</title>
+    <title>Auto Refresh Page</title>
 </head>
 <body>
-    <script type="application/javascript">
-        pn_idzone = 5455362;
-        pn_sleep_seconds = 0;
-        pn_is_self_hosted = 0;
-        pn_soft_ask = 1;
-        pn_filename = "/worker.js";
-        pn_soft_ask_horizontal_position = "left";
-        pn_soft_ask_vertical_position = "top";
-        pn_soft_ask_title_enabled = 1;
-        pn_soft_ask_title = "Click ALLOW to continue";
-        pn_soft_ask_description = "Would you like to receive great special offers & promotions?";
-        pn_soft_ask_yes = "ALLOW";
-        pn_soft_ask_no = "NO, THANKS"; 
-    </script>
-    <script type="application/javascript" src="https://js.wpnsrv.com/pn.php"></script>
+    <script async type="application/javascript" src="https://a.magsrv.com/ad-provider.js"></script>
+    <ins class="eas6a97888e2" data-zoneid="5455366"></ins>
+    <script>(AdProvider = window.AdProvider || []).push({"serve": {}});</script>
 </body>
 </html>
 '''
@@ -36,10 +24,6 @@ html_template = '''
 @app.route('/')
 def index():
     return render_template_string(html_template)
-
-@app.route('/worker.js')
-def worker_js():
-    return Response('importScripts("https://js.wpnsrv.com/worker.php?v=2.0");', mimetype='application/javascript')
 
 def open_browser():
     # Wait a moment to ensure the server is up
@@ -49,6 +33,6 @@ def open_browser():
 if __name__ == '__main__':
     # Start the Flask server in a separate thread
     threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
-
+    
     # Open the browser
     open_browser()
