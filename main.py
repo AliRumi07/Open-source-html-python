988b0a794a05141ad93aa811c10d66ea983a9e4d
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-10-29 17:44:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,15 @@ import webbrowser
 
 app = Flask(__name__)
 
-# HTML template with auto-refresh meta tag and the new ad code
+# Simple HTML template with "Hello World"
 html_template = '''
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="refresh" content="300">
-    <title>Auto Refresh Page</title>
+    <title>Hello World</title>
 </head>
 <body>
-    <script async type="application/javascript" src="https://a.magsrv.com/ad-provider.js"></script>
-    <ins class="eas6a97888e2" data-zoneid="5455366"></ins>
-    <script>(AdProvider = window.AdProvider || []).push({"serve": {}});</script>
+    <h1><!-- Bidvertiser2096140 --></h1>
 </body>
 </html>
 '''
@@ -26,13 +23,9 @@ def index():
     return render_template_string(html_template)
 
 def open_browser():
-    # Wait a moment to ensure the server is up
     time.sleep(1)
     webbrowser.open_new('http://0.0.0.0:8080')
 
 if __name__ == '__main__':
-    # Start the Flask server in a separate thread
     threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
-    
-    # Open the browser
     open_browser()
