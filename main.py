5307341b61cf683c1a364c363da5adc5b707dcd3
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-10-29 17:48:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template_string
-import threading
-import time
-import webbrowser
 
 app = Flask(__name__)
 
-# Simple HTML template with "Hello World"
 html_template = '''
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hello World</title>
+    <title>Home</title>
 </head>
 <body>
-    <h1><!-- Bidvertiser2096140 --></h1>
+    <!-- Bidvertiser2096141 -->
 </body>
 </html>
 '''
@@ -22,10 +18,5 @@ html_template = '''
 def index():
     return render_template_string(html_template)
 
-def open_browser():
-    time.sleep(1)
-    webbrowser.open_new('http://0.0.0.0:8080')
-
 if __name__ == '__main__':
-    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
-    open_browser()
+    app.run(host='0.0.0.0', port=8080)
