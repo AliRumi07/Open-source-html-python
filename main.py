1477704466e91b650b7cacbabf9c63f6335418ee
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-10-30 22:34:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-html_template = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Centered Content</title>
-    <meta name="monetag" content="a3a439a786f9e23dbf22ef8b67a94837">
-    <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            min-height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        .content {
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <div class="content">
-        <script type="text/javascript">
-            aclib.runAutoTag({
-                zoneId: 'xzqj5qrw84',
-            });
-        </script>
-    </div>
-</body>
-</html>
-'''
-
 @app.route('/')
 def hello_world():
-    return render_template_string(html_template)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
